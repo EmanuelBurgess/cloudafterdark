7412af768687184ad587220dbc3ea914b64dd95e
tmahone5 <mahoneandson55@aol.com>
2020-11-28 00:55:42 -0600

    homework. Bash script using GLoabla variables

---
+++ 11.24Hmwrk.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+rapper=$1
+rapper="Common"
+DJ=$1
+DJ="DJ Premiere"
+player1=$1 player2=$2 player3=$3
+player1="Michael Jordan" player2="Scottie Pippen" player3="James Worthy"
+echo My favorite rapper is $rapper
+echo My favorite DJ is $DJ
+echo My top 3 NBA players are $player1, $player2, $player3
+
+
+
